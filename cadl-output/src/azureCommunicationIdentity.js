a1774a71bfd7c382f6414175945c30b94b54fa01
Petr Švihlík <petrsvihlik@microsoft.com>
2022-10-21 18:11:35 +0200

    x

--- cadl-output/src/azureCommunicationIdentity.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Initialize a new instance of the class AzureCommunicationIdentityClient class.
- *
- */
-function createClient(options = {}) { }
-exports.default = createClient;
